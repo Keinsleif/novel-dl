0acd34794898e5c1e6535b44d37d0693d266f2be
kazuto28 <konndoukazuto45@gmail.com>
2021-06-18 14:43:47 +0900

    kakuyomu css generater

--- novel_dl/themes/kakuyomu/tool/css_setup.sh
+++ novel_dl/themes/kakuyomu/tool/css_setup.sh
@@ -1,7 +1,13 @@
 TARGET=$1
-purifycss kakuyomu-old.css t.html ../*.html ../static/kakuyomu.js --out ${TARGET}
+if [ $# -ne 1 ]; then
+    exit
+fi
+ROOT=$(cd $(dirname $0); pwd)
+curl -#L -o /tmp/kakuyomu-old.css https://cdn-static.kakuyomu.jp/css/kakuyomu.css
+purifycss /tmp/kakuyomu-old.css ${ROOT}/template.html ${ROOT}/../*.html ${ROOT}/../static/kakuyomu.js --out ${TARGET} --info
 sed -e "s@/font/dcsymbols-regular.woff?6tnyRgzhZFkJ@dcsymbols-regular.woff@" -i ${TARGET}
 sed -e "s@/font/dcsymbols-regular.otf?VjSwft_YX8ck@dcsymbols-regular.otf@" -i ${TARGET}
 sed -e "s@/font/dcicons-regular.eot?OicIuNS6IwQE@dcicons-regular.eot@g" -i ${TARGET}
 sed -e "s@/font/dcicons-regular.otf?woyfFEfr8EbA@dcicons-regular.otf@" -i ${TARGET}
 sed -e "s@.isTouch #globalHeaderTouch-globalNav img@.isTouch #globalHeaderTouch-globalNav svg@" -i ${TARGET}
+rm -f /tmp/kakuyomu-old.css
